a0f87e89cd64c7fb9a3e2d8398bf3bf1173540fe
Piyushnmj <piyushnmj@yahoo.com>
2023-01-07 15:59:06 +0530

    Setting up the ASP.Net Core Web API Project and Configuring

---
+++ FundooNoteApplication/RepositoryLayer/Context/FundooContext.cs
@@ -0,0 +1,17 @@
+﻿using CommonLayer.Model;
+using Microsoft.EntityFrameworkCore;
+using RepositoryLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Context 
+{
+    public class FundooContext : DbContext
+    {
+        public FundooContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<UEntity> UserTable { get; set; }
+    }
+}
